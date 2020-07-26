5dbbed4f30ecbddedd4874aa31e192bebfe6d490
mattamo90 <matt.amo90@gmail.com>
2020-07-26 12:27:42 +0000

    font changes for heading, plus fall back fonts added. Amendments to map js to try to get multiple locations showing

---
+++ assets/js/map.js
@@ -0,0 +1,33 @@
+ <script>
+    function initMap() {
+  // The location of Uluru
+  var snowdon = {lat: 53.0680, lng: -4.0720};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: snowdon});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: snowdon, map: map});
+}
+// Initialize and add the map
+
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3voYuJPggNKICI88qdzDmOLuXqaSuKps&callback=initMap">
+    </script>
+    <script src="assets/js/map.js"></script> <script>
+    function initMap() {
+  // The location of Uluru
+  var snowdon = {lat: 53.0680, lng: -4.0720};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: snowdon});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: snowdon, map: map});
+}
+// Initialize and add the map
+
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3voYuJPggNKICI88qdzDmOLuXqaSuKps&callback=initMap">
+    </script>
+    <script src="assets/js/map.js"></script>
